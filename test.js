41253d7cc0216d000e55a44aae92a8dbee5afcae
Connor Duggan <connor.duggan28@yahoo.com>
2023-01-23 20:21:53 -0500

    writing some psuedo framework to have an idea of what i need going into it

---
+++ test.js
@@ -0,0 +1,7 @@
+// Scoreboard for players
+
+// line of interactable buttons that when clicked drop a piece in the corresponding column
+
+// 7 x 6 board with yellow or red circles marked after a player makes a move in the section above
+
+
